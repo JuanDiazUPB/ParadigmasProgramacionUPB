a7a1a4140770f93a28913031218611cb616d907e
Juan Díaz <80717100+JuanDiazUPB@users.noreply.github.com>
2021-09-25 21:50:49 -0500

    Add files via upload

---
+++ Laboratorios/C#/Laboratorio POO/Cuenta.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_POO
+{
+    class Cuenta
+    {
+        // Atributos
+        private int numero;
+        private String nombre;
+        private double saldo;
+        private String tipo;
+
+        public Cuenta()
+        {
+            Random rnd = new Random();
+            numero = rnd.Next(10000000, 99999999);
+
+        }
+
+        // Get y set
+        public int getNumero()
+        {
+            return this.numero;
+        }
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+        public void setTipo(String tipo)
+        {
+            this.tipo = tipo;
+        }
+        public void setSaldo(double saldo)
+        {
+            this.saldo = saldo;
+        }
+        public double getSaldo()
+        {
+            return this.saldo;
+        }
+
+        // Métodos
+        public void consignarDinero(double consigna)
+        {
+            if (consigna > 0)
+            {
+                this.saldo += consigna;
+                Console.WriteLine("Su saldo después de la consigna es: " + saldo);
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un valor válido");
+            }
+        }
+
+        public void retirarDinero(double retiro)
+        {
+            if (retiro <= this.saldo)
+            {
+                this.saldo -= retiro;
+                Console.WriteLine("Su saldo después del retiro es: " + saldo);
+            }
+            else
+            {
+                Console.WriteLine("Ingrese un valor válido");
+            }
+        }
+
+        public void verSaldo()
+        {
+            Console.WriteLine("\nSu saldo actual es: " + saldo);
+        }
+
+        public void verDatos()
+        {
+            Console.WriteLine("\nNombre: {0}\nNúmero de cuenta: {1}\nTipo de cuenta: {2}\nSaldo de la cuenta: {3}"
+                , nombre, numero, tipo, saldo);
+        }
+    }
+}
---
+++ Laboratorios/C#/Laboratorio POO/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta();
+
+            Console.WriteLine("Ingrese su nombre:");
+            String n = Console.ReadLine();
+            cuenta1.setNombre(n);
+            Console.WriteLine("\nBienvenido {0}, su número de cuenta es {1}", cuenta1.getNombre(), cuenta1.getNumero());
+
+            Console.WriteLine("\nSeleccione el tipo de cuenta (escriba el número):\n\n1. Ahorros\n2. Corriente");
+            while (true)
+            {
+                string t = Console.ReadLine();
+                if (t == "1")
+                {
+                    cuenta1.setTipo("Ahorros");
+                    cuenta1.setSaldo(50000);
+                    Console.WriteLine("\nLa cuenta seleccionada ha sido 'Ahorros'");
+                    break;
+                }
+                else if (t == "2")
+                {
+                    cuenta1.setTipo("Corriente");
+                    cuenta1.setSaldo(100000);
+                    Console.WriteLine("\nLa cuenta seleccionada ha sido 'Corriente'");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nDigite un valor válido\n");
+                }
+            }
+            
+            while (true)
+            {
+                Console.WriteLine("\nQué acción quiere realizar?\n\n1. Consignar dinero\n2. Retirar dinero\n3. Ver saldo\n4. Ver todos los datos\n5. Salir");
+                var accion = Console.ReadLine();
+                if (accion == "1")
+                {
+                    Console.WriteLine("\nCuánto dinero quiere consignar? (Su saldo actual es: {0})", cuenta1.getSaldo());
+                    double consigna = double.Parse(Console.ReadLine());
+                    cuenta1.consignarDinero(consigna);
+                }
+                if (accion == "2")
+                {
+                    Console.WriteLine("\nCuánto dinero quiere retirar? (Su saldo actual es: {0})", cuenta1.getSaldo());
+                    double retiro = double.Parse(Console.ReadLine());
+                    cuenta1.retirarDinero(retiro);
+                }
+                if (accion == "3")
+                {
+                    cuenta1.verSaldo();
+                }
+                if (accion == "4")
+                {
+                    cuenta1.verDatos();
+                }
+                if (accion == "5")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
